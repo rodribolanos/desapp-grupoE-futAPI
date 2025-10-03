3156d0ffad87da5245bffc8b7c25cc50a8dcabb0
Rodrigo Bolaños <rodrimanuelbw@gmail.com>
2025-10-03 17:49:24 -0300

    renames

--- src/main/kotlin/ar/edu/unq/futapp/service/TeamApiClient.kt
+++
@@ -1,8 +0,0 @@
-package ar.edu.unq.futapp.service
-
-import ar.edu.unq.futapp.model.Team
-import java.util.Optional
-
-interface TeamApiClient {
-    fun findTeam(teamName: String): Optional<Team>
-}
--- src/main/kotlin/ar/edu/unq/futapp/service/impl/TeamServiceImpl.kt
+++ src/main/kotlin/ar/edu/unq/futapp/service/impl/TeamServiceImpl.kt
@@ -3,14 +3,14 @@ package ar.edu.unq.futapp.service.impl
 import ar.edu.unq.futapp.exception.EntityNotFound
 import ar.edu.unq.futapp.model.Player
 import ar.edu.unq.futapp.model.Team
-import ar.edu.unq.futapp.service.TeamApiClient
+import ar.edu.unq.futapp.service.WhoScoredApiClient
 import ar.edu.unq.futapp.service.TeamService
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
 import java.util.Optional
 
 @Service
-class TeamServiceImpl @Autowired constructor(val teamApiClient: TeamApiClient): TeamService {
+class TeamServiceImpl @Autowired constructor(val teamApiClient: WhoScoredApiClient): TeamService {
     override fun findPlayersByTeam(teamName: String): List<Player> {
         val team: Optional<Team> = teamApiClient.findTeam(teamName)
         if(team.isEmpty) throw EntityNotFound("Team with name $teamName not found")
