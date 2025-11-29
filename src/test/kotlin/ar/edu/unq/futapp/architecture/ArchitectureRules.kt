5113c7cac5b6bba595219056fbfc02137f38651c
Cristopher Bechtholdt <cristopherbechtholdt@gmail.com>
2025-11-29 02:46:48 -0300

    Exclude tests from architecture tests

--- src/test/kotlin/ar/edu/unq/futapp/architecture/ArchitectureRules.kt
+++ src/test/kotlin/ar/edu/unq/futapp/architecture/ArchitectureRules.kt
@@ -2,11 +2,14 @@ package ar.edu.unq.futapp.architecture
 
 import com.tngtech.archunit.core.domain.JavaClasses
 import com.tngtech.archunit.core.importer.ClassFileImporter
+import com.tngtech.archunit.core.importer.ImportOption
 import com.tngtech.archunit.library.Architectures
 import org.junit.jupiter.api.Test
 
 class ArchitectureRules {
-    private val classes: JavaClasses = ClassFileImporter().importPackages("ar.edu.unq.futapp")
+    private val classes: JavaClasses = ClassFileImporter()
+        .withImportOption(ImportOption.DoNotIncludeTests())
+        .importPackages("ar.edu.unq.futapp")
 
     @Test
     fun correctServiceDependencies() {
